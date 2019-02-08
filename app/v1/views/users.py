18c19ff9386b94ee7aafb47777d899c78898c672
Geofrey Boro <ateeyniboro@gmail.com>
2019-02-08 21:48:21 +0300

    [Finishes#163717488] User able to get all political parties

---
+++ app/v1/views/users.py
@@ -0,0 +1,25 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from app.v1.utilities.validator import response, exists
+from app.v1.models.user_model import User
+from app.v1.blueprints import bp
+
+
+users_list = User.users
+
+
+
+
+@bp.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+
+    model = User()
+    data = model.find_by_id(id)
+
+    if not data:
+        return response('User not found', 404)
+
+    return response('Request sent successfully', 200, [data])
+
