5e3fcff362e60487137a9f6c5eb538b345cf966e
Geofrey Boro <ateeyniboro@gmail.com>
2019-02-08 22:04:37 +0300

    [Finishes#163717961] Admin should be able to create political office

---
+++ app/v1/views/offices.py
@@ -0,0 +1,46 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from app.v1.utilities.validator import response, exists
+from app.v1.models.office_model import Office
+from app.v1.blueprints import bp
+
+
+office_list = Office.offices
+
+
+@bp.route('/offices', methods=['POST', 'GET'])
+def create_office():
+    if request.method == 'POST':
+        """ Create office end point """
+
+        data = request.get_json() 
+
+        if not data:
+            return response("No data was provided", 400)
+
+        try:
+            typ = data['type']
+            name = data['name']
+        except KeyError as e:
+            return response("{} field is required".format(e.args[0]), 400)
+
+        office = Office(name, typ)
+
+        if not office.validate_object():
+            return response(office.error_message, office.error_code)
+
+        # append new office to list
+        office.save()
+
+        # return added office
+        return response("Office created successfully", 201, [office.as_json()])
+
+    elif request.method == 'GET':
+        """ Get all offices end point """
+
+        return response('Request was sent successfully', 200, office_list)
+
+
+
